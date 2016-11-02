9d06eaa2dd8968852ee3f40b587be2d383d186e2
moonou <moonoulong@gmail.com>
2016-11-02 15:20:28 +0800

    Mod: change jeet and postcss to unit test

--- build/utils.js
+++ build/utils.js
@@ -40,7 +40,7 @@ exports.cssLoaders = function (options) {
     postcss: generateLoaders(['css']),
     less: generateLoaders(['css', 'less']),
     sass: generateLoaders(['css', 'sass?indentedSyntax']),
-    scss: generateLoaders(['css', 'sass', 'postcss']),
+    scss: generateLoaders(['css', 'postcss', 'sass']),
     stylus: generateLoaders(['css', 'stylus']),
     styl: generateLoaders(['css', 'stylus'])
   }
